3823dd7c44e1faae6e2649149a5338ae55454cfd
Anatoly Ryabov <m1neral@yandex.ru>
2016-06-03 19:37:37 +0300

    Create list of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    render :show
+  end
+
+  def show
+    @category = Category.find(params[:id])
   end
 end
