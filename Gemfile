f0a2fbe3ccc0c9cf58e5718ddd11976536ba315c
Anatoly Ryabov <m1neral@yandex.ru>
2016-06-09 18:20:26 +0300

    Add some features to reviews / manage reviews

--- Gemfile
+++ Gemfile
@@ -16,6 +16,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+  gem 'awesome_print'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-remote'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,29 @@
 class ReviewsController < ApplicationController
-  before_action :get_product, only: [:create]
+  before_action :set_product, only: [:create, :destroy]
+  before_action :set_review, only: [:destroy]
 
   def create
     @review = @product.reviews.new(review_params)
     if @review.save
-      redirect_to(@product)
+      redirect_to(@product, flash: {added: t('.added')})
     else
       render 'products/show'
     end
   end
 
+  def destroy
+    @review.destroy
+    redirect_to(@product, flash: {removed: t('.removed')})
+  end
+
   private
 
-  def get_product
+  def set_product
     @product = Product.find(params[:product_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 
   def review_params
